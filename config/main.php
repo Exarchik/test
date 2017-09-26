bed9aebdddce68ac6040d707fe598dfb17c7ddf3
Dmytro <exarchik@gmail.com>
2017-09-26 21:48:57 +0300

    test

--- config/main.php
+++ config/main.php
@@ -6,8 +6,9 @@
  */
 class MainConfig{
     
+	// basic url 
     static public $baseUrl = "http://localhost/test";
-	
+	// basic dir
     static public $baseDir = "C:\\OpenServer\\OpenServer\\domains\\localhost\\test";
     
 }
